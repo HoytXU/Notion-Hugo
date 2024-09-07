254aab43951a0fbe89768f94952a6f466c099a58
Hoyt <74821063+HoytXU@users.noreply.github.com>
2024-09-07 10:04:16 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,14 +3,14 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://www.notion.so/2a001c8b88e44ecab7def1e35e36fd73?v=8a902db4fd8b457dba4abbe210659bd0',
+        page_url: 'https://www.notion.so/2a001c8b88e44ecab7def1e35e36fd73',
         pages: [
             // {
             //     page_id: '<page_id>',
             //     target_folder: 'path/relative/to/content/folder'
             // }
             {
-                page_id: '45eb121158b9489480ec000fd25c812b',
+                page_id: '2d07d713507545e3a2c400c63857d09f',
                 target_folder: '.'
             }
         ],
@@ -20,7 +20,7 @@ const userConfig: UserConfig = {
             //     target_folder: 'path/relative/to/content/folder'
             // }
             {
-                database_id: 'b7b1816c05ec464391c8c111fa242985',
+                database_id: '2a001c8b88e44ecab7def1e35e36fd73',
                 target_folder: '.'
             }
         ],
